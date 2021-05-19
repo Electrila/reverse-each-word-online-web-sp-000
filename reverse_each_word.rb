2fd6ad1d8bedde3a46e4ac41e3b97ef328154d9f
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-19 20:54:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,3 @@
 def reverse_each_word(string)
-  array = string.split(" ")
-  reversed_array = array.collect { |word| word.reverse }
-  reversed_string = reversed_array.join(" ")
-  reversed_string
+  string.split(" ").collect { |word| word.reverse }.join(" ")
 end
